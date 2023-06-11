a1e70593f8a5512315d4ac9869d7e8e028532a58
ELFATIMI SALAHEDDINE <salahfatimi16@gmail.com>
2023-06-11 13:55:54 +0100

    add jwt

--- Back_end/database/seeders/controlerlogin.php
+++ Back_end/database/seeders/controlerlogin.php
@@ -14,26 +14,26 @@ class controlerlogin extends Seeder
      */
     public function run(): void
     {
-        controlerlogins::create([
-            'username'=>'user1',
-            'password'=>'123',
-            'idCard'=>'EE1234',
-            'token'=> Str::random(10),
+        // controlerlogins::create([
+        //     'username'=>'user1',
+        //     'password'=>'123',
+        //     'idCard'=>'EE1234',
+        //     'token'=> Str::random(10),
 
-        ]);
-        controlerlogins::create([
-            'username'=>'user2',
-            'password'=>'123',
-            'idCard'=>'EE12345',
-            'token'=> Str::random(10),
+        // ]);
+        // controlerlogins::create([
+        //     'username'=>'user2',
+        //     'password'=>'123',
+        //     'idCard'=>'EE12345',
+        //     'token'=> Str::random(10),
 
-        ]);
-        controlerlogins::create([
-            'username'=>'user3',
-            'password'=>'123',
-            'idCard'=>'EE123456',
-            'token'=> Str::random(10),
+        // ]);
+        // controlerlogins::create([
+        //     'username'=>'user3',
+        //     'password'=>'123',
+        //     'idCard'=>'EE123456',
+        //     'token'=> Str::random(10),
 
-        ]);
+        // ]);
     }
 }
--- Back_end/database/seeders/controlerregester.php
+++ Back_end/database/seeders/controlerregester.php
@@ -13,32 +13,32 @@ class controlerregester extends Seeder
      */
     public function run(): void
     {
-        controlerregesters::create([
-            'fullName'=>'user1',
-            'idCard'=>'EE1234',
-            'Address'=>'adressse  lien',
-            'speciality'=>'Plomberie',
-            'phoneNum'=>'0602314804',
-            'email'=>'user1@gmail.com',
-            'dateStart'=>'2023-03-26',
-        ]);
-        controlerregesters::create([
-            'fullName'=>'user2',
-            'idCard'=>'EE12345',
-            'Address'=>'adressse  lien',
-            'speciality'=>'Electricite',
-            'phoneNum'=>'0602314804',
-            'email'=>'user2@gmail.com',
-            'dateStart'=>'2023-01-26',
-        ]);
-        controlerregesters::create([
-            'fullName'=>'user3',
-            'idCard'=>'EE123456',
-            'Address'=>'adressse  lien',
-            'speciality'=>'Climatisation',
-            'phoneNum'=>'0602312345',
-            'email'=>'user3@gmail.com',
-            'dateStart'=>'2023-02-26',
-        ]);
+        // controlerregesters::create([
+        //     'fullName'=>'user1',
+        //     'idCard'=>'EE1234',
+        //     'Address'=>'adressse  lien',
+        //     'speciality'=>'Plomberie',
+        //     'phoneNum'=>'0602314804',
+        //     'email'=>'user1@gmail.com',
+        //     'dateStart'=>'2023-03-26',
+        // ]);
+        // controlerregesters::create([
+        //     'fullName'=>'user2',
+        //     'idCard'=>'EE12345',
+        //     'Address'=>'adressse  lien',
+        //     'speciality'=>'Electricite',
+        //     'phoneNum'=>'0602314804',
+        //     'email'=>'user2@gmail.com',
+        //     'dateStart'=>'2023-01-26',
+        // ]);
+        // controlerregesters::create([
+        //     'fullName'=>'user3',
+        //     'idCard'=>'EE123456',
+        //     'Address'=>'adressse  lien',
+        //     'speciality'=>'Climatisation',
+        //     'phoneNum'=>'0602312345',
+        //     'email'=>'user3@gmail.com',
+        //     'dateStart'=>'2023-02-26',
+        // ]);
     }
 }
