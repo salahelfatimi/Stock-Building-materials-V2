85038398c639951ab44f1e19b983102a0304b410
Hamza Outmassint <hamzaayrom@gmail.com>
2024-02-18 22:58:53 +0100

    i remove few items from designation seeder

--- Back_end/database/seeders/designations.php
+++ Back_end/database/seeders/designations.php
@@ -67,111 +67,6 @@ class designations extends Seeder
             'speciality'=>'Electricite'
         ]);
         designation::create([
-            'designationName'=>"Tubage 13",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Cable Rigide 1,5",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Cable Rigide 2,5",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Souple 2.5 Clim",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Cable Coaxiale",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Cable Ethernet Cat6",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Prise Electrique",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Prise TV/Info/Telephone",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Prise Etanche",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Interrupteur Simple",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Interrupteur Double",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Interrupteur Va et Vient",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Interrupteur Double Va et vien",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Interrupteur Volet Roulant",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Bouton Poussoir",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Spot",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Lustre",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"DDR",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Idiff",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"DD10A",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"DD16A",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"DD25A",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"DD32A",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Telerupteur",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Minuterie",
-            'speciality'=>'Electricite'
-        ]);
-        designation::create([
-            'designationName'=>"Parafoudre",
-            'speciality'=>'Electricite'
-        ]);
-
-        designation::create([
             'designationName'=>"Cuivre 1/4 + 3/8",
             'speciality'=>'Climatisation'
         ]);
@@ -197,34 +92,6 @@ class designations extends Seeder
         ]);
         designation::create([
             'designationName'=>"Alimentation Electrique",
-            'speciality'=>'Climatisation'
-        ]);
-        designation::create([
-            'designationName'=>"Condensat",
-            'speciality'=>'Climatisation'
-        ]);
-        designation::create([
-            'designationName'=>"Thermostat",
-            'speciality'=>'Climatisation'
-        ]);
-        designation::create([
-            'designationName'=>"Split 7kw",
-            'speciality'=>'Climatisation'
-        ]);
-        designation::create([
-            'designationName'=>"Split 5.6kw",
-            'speciality'=>'Climatisation'
-        ]);
-        designation::create([
-            'designationName'=>"Cassette 14kw",
-            'speciality'=>'Climatisation'
-        ]);
-        designation::create([
-            'designationName'=>"Cassette 10kw",
-            'speciality'=>'Climatisation'
-        ]);
-        designation::create([
-            'designationName'=>"Cassette 7kw",
             'speciality'=>'Climatisation'
         ]);
         designation::create([
@@ -255,34 +122,5 @@ class designations extends Seeder
             'designationName'=>"Raccord PER",
             'speciality'=>'Plomberie'
         ]);
-        designation::create([
-            'designationName'=>"Vanne",
-            'speciality'=>'Plomberie'
-        ]);
-        designation::create([
-            'designationName'=>"Clapet Anti-retour",
-            'speciality'=>'Plomberie'
-        ]);
-        designation::create([
-            'designationName'=>"Coffret collecteu",
-            'speciality'=>'Plomberie'
-        ]);
-        designation::create([
-            'designationName'=>"Chaudiere",
-            'speciality'=>'Plomberie'
-        ]);
-        designation::create([
-            'designationName'=>"Balon de Stockage",
-            'speciality'=>'Plomberie'
-        ]);
-        designation::create([
-            'designationName'=>"Chauffe eau solaire",
-            'speciality'=>'Plomberie'
-        ]);
-        designation::create([
-            'designationName'=>"Pompe",
-            'speciality'=>'Plomberie'
-        ]);
-
     }
 }
